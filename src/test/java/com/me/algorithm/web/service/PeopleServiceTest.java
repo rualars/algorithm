3849e2638e4c885932227040bd640acb16c4b860
rualars <shuxiaor@163.com>
2020-12-09 22:42:33 +0800

    --

--- src/test/java/com/me/algorithm/web/service/PeopleServiceTest.java
+++ src/test/java/com/me/algorithm/web/service/PeopleServiceTest.java
@@ -1,5 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.me.algorithm.web.service;
 
+import com.alibaba.fastjson.JSON;
+import com.me.algorithm.AlgorithmApplication;
+import com.me.algorithm.web.dto.TestResponseDTO;
+import com.me.algorithm.web.entity.People;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AlgorithmApplication.class)
 class PeopleServiceTest {
+    private static final Logger logger = LoggerFactory.getLogger(PeopleServiceTest.class);
 
+    @Autowired
+    private PeopleService peopleService;
+
+    @Test
+    void getPeopleById() {
+        TestResponseDTO<People> dto = peopleService.getPeopleById(1);
+        logger.info(JSON.toJSONString(dto));
+    }
 }
